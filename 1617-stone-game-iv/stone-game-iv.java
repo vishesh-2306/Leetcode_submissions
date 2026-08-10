06964249cd0f59a2c833b6662ef59bf0c15c5434
Vishesh Kaushal <visheshkaushal8@gmail.com>
2026-08-10 14:16:33 +0530

    Time: 55 ms (22.45%) | Memory: 42.3 MB (83.55%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n+1] ;   
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j*j <= i ; j++){
+                if( !dp[i - j*j] ) dp[i] = true ;
+            }
+        }
+
+        return dp[n] ;
+    }
+}
