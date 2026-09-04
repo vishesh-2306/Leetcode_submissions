611a1ff9bb14650e64936f9821268a5f3181e93f
Vishesh Kaushal <visheshkaushal8@gmail.com>
2026-09-04 11:32:07 +0530

    Time: 2 ms (11.49%) | Memory: 46.1 MB (88.30%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,14 +1,20 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int max = 0 ;
+        Deque<Integer> q = new ArrayDeque<>() ;
+        for(int i = 0 ; i < nums.length ; i++){
+            while( !q.isEmpty() && nums[q.peekLast()] > nums[i] ) q.pollLast() ;
+
+            q.addLast(i) ;
+
+        }
 
         for(int i = 0 ; i < nums.length ; i++){
             max = Math.max(nums[i],max) ;
-            int min = nums[i] ;
-            for(int j = i ; j < nums.length ; j++){
-                min = Math.min(nums[j],min) ;
-            }
-            if( max - min <= k ) return i ;
+            if( q.peek() < i ) q.poll() ;
+
+            if( max - nums[q.peek()] <= k ) return i ;
+            
         }
 
         return -1 ;
